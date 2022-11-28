0b7b6374547371ce25a0eeb7898f13f24361f9c5
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2022-11-27 22:49:38 -0600

    Update Astro config with base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://cassidoo.co",
+	base: "/blog",
 	integrations: [sitemap()],
 });
