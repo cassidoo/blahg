77d762b1967eb9fb3405621aa4cc98077a89a4d3
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-01-26 23:40:45 -0600

    Update posts by tag, fix ul styling

---
+++ src/scripts/getTags.js
@@ -0,0 +1,23 @@
+export default function getTags(posts) {
+	// Get tags from all posts
+	const allTags = posts
+		.map((post) => {
+			const postTags = post.frontmatter.tags;
+			let allTags = [];
+
+			if (postTags.length > 0) {
+				postTags.forEach((tag) => {
+					if (allTags.indexOf(tag) === -1) {
+						allTags.push(tag);
+					}
+				});
+			}
+			return allTags;
+		})
+		.flat(1);
+
+	// Make the tags unique
+	let tags = [...new Set(allTags)];
+
+	return tags;
+}
