10f6a08ce3ea9bc5f6f55ec4c422ab6cfe31a096
kimblim <github@kimjohannesen.dk>
2024-12-30 11:37:18 +0100

    First working dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "kimjohannesendk.netlify.app",
+	site: "https://kimjohannesendk.netlify.app",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
