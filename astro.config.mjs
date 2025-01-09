fffd869f1fd4da54fc069342c728373d9038285f
tshepiso <tshepi.tech@gmail.com>
2025-01-09 12:08:25 +0100

    initial setup for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg.netlify.app/",
+	site: "https://techindoses.netlify.app/",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
